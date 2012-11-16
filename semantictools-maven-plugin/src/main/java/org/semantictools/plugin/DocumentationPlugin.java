6a0000a2e310a37422bea69d5df06c41860ee72c
Greg McFall <gregory.mcfall@gmail.com>
2012-11-16 11:54:14 -0600

    Adds Apache 2.0 License file

--- semantictools-maven-plugin/src/main/java/org/semantictools/plugin/DocumentationPlugin.java
+++ semantictools-maven-plugin/src/main/java/org/semantictools/plugin/DocumentationPlugin.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2012 UMCFAG1
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.semantictools.plugin;
 
 import java.io.File;
@@ -15,7 +30,7 @@ import org.semantictools.publish.DocumentationGenerator;
  * @goal generate
  * 
  * @phase generate-sources
- * @author Greg McFall
+ * @author Greg McFall 
  *
  */
 public class DocumentationPlugin extends AbstractMojo {
